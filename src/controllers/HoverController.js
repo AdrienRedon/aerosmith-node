dfc529a6dbb6c7ff4a90eaa04ea22b161fe8513e
Adrien REDON <redon.adrien@gmail.com>
2018-01-21 10:16:55 +0100

    add build tooling and telnet command to hover

--- src/controllers/HoverController.js
+++ src/controllers/HoverController.js
@@ -1,14 +1,11 @@
+
 const Control = require('../control');
 
 class HoverController {
   static takeOff() {
     console.log('taking off ...');
 
-    const ref = {
-      fly: true,
-      emergency: false,
-    };
-    Control.execute(ref);
+    exec('cat hover | telnet 192.168.1.1');
   }
 
   static land() {
@@ -20,11 +17,6 @@ class HoverController {
     Control.execute(ref);
   }
 
-  static blinkLed() {
-    console.log('leds blinking');
-
-    Control.blinkLed();
-  }
 }
 
 module.exports = HoverController;
