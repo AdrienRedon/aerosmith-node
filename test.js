36076d746cf96b59c78792b0f3d1d5bb5b33e71b
Adrien REDON <redon.adrien@gmail.com>
2018-01-21 09:21:55 +0100

    add test gpio raspberry

---
+++ test.js
@@ -0,0 +1,15 @@
+var Gpio = require('pigpio').Gpio,
+  motor = new Gpio(18, {mode: Gpio.OUTPUT}),
+  pulseWidth = 1000,
+  increment = 100;
+
+setInterval(function () {
+  motor.servoWrite(pulseWidth);
+
+  pulseWidth += increment;
+  if (pulseWidth >= 2000) {
+    increment = -100;
+  } else if (pulseWidth <= 1000) {
+    increment = 100;
+  }
+}, 1000);
